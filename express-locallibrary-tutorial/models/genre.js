e4a4dc04fa4b4f6370e89a875fc38132acccfb58
Saadat-B <saadatbadgujar@gmail.com>
2024-07-16 11:22:32 +0530

    view templates added

--- express-locallibrary-tutorial/models/genre.js
+++ express-locallibrary-tutorial/models/genre.js
@@ -1,18 +1,16 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Book = require("../models/book");
 
 const Schema = mongoose.Schema;
 
-const GenreSchema = new Schema(
-    {
-        name: { type: String, required: true, minlength: 3, maxlength: 100 },
-    }
-);
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 100 },
+});
 
 // Virtual for genre's URL
-GenreSchema.virtual('url').get(function() {
-    return '/genre/' + this._id;
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
 });
 
 // Export the model
-module.exports = mongoose.model('Genre', GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
