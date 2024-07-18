0c38d72c7fab58469785831a326bd31914f6348c
Saadat-B <saadatbadgujar@gmail.com>
2024-07-18 12:05:10 +0530

    add book create get controller

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -63,7 +63,16 @@ exports.book_detail = asyncHandler(async (req, res, next) => {
 
 // Display book create form on GET.
 exports.book_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create GET");
+  const [allAuthors, allGenres] = await Promise.all([
+    Author.find().sort({ family_name: 1 }).exec(),
+    Genre.find().sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("book_form", {
+    title: "Create Book",
+    authors: allAuthors,
+    genres: allGenres,
+  });
 });
 
 // Handle book create on POST.
